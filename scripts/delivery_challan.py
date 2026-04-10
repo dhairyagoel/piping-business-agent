729ad0e315882824b25c0d2f12c07f32a6449629
dhairyagoel <74338524+dhairyagoel@users.noreply.github.com>
2026-04-10 15:21:16 +0530

    Add scripts/delivery_challan.py

---
+++ scripts/delivery_challan.py
@@ -0,0 +1,4 @@
+"""
+Gelivery challan (B Form)  - Automatic stock deduction
+Based on (msos statump"This provides a B / ABC form Compliant Delivery Challan. "
+    "Do not euse for mass/scaled generation. For those, use A / T (Challan Mode).",
